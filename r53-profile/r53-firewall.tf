2695241f161ff87801bfb4d9f43d9bef2c98a6cf
Venkata Para <venkata.para@digital.homeoffice.gov.uk>
2025-04-07 16:47:32 +0100

    corrected syntax for the firewall commands

--- r53-profile/r53-firewall.tf
+++ r53-profile/r53-firewall.tf
@@ -15,14 +15,14 @@ resource "aws_route53_resolver_firewall_rule" "block_rules" {
   name                  = replace(each.key, ".", "-")
   action                = "BLOCK"
   block_response        = "NXDOMAIN"
-  firewall_domain_list_id = aws_route53_resolver_dns_firewall_domain_list.blocked_domains.id
+  firewall_domain_list_id = aws_route53_resolver_firewall_domain_list.blocked_domains.id
   priority              = index(split("\n", file(var.domain_file_path)), each.key) + 1
-  firewall_rule_group_id = aws_route53_resolver_dns_firewall_rule_group.rule_group.id
+  firewall_rule_group_id = aws_route53_resolver_firewall_rule_group.rule_group.id
 }
 
 resource "aws_route53_resolver_firewall_rule_group_association" "assoc" {
   name                    = "${var.rule_group_name}-assoc"
-  firewall_rule_group_id  = aws_route53_resolver_dns_firewall_rule_group.rule_group.id
+  firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.rule_group.id
   priority                = var.association_priority
   vpc_id                  = var.vpc_id
   mutation_protection     = "DISABLED"
