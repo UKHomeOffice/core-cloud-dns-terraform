0447ecdfd7e3d343c3f07b6707de533df01d0a2b
Vijay Jadhav <mail2vjadhav@gmail.com>
2025-05-15 10:29:05 +0100

    HybridDNS: Adding private natg and creating new rt

--- modules/r53-profile/r53-profile-network.tf
+++ modules/r53-profile/r53-profile-network.tf
@@ -28,6 +28,19 @@ locals {
 # Create Resolver Subnets
 ##############################
 
+resource "aws_subnet" "cc_inbound_endpoint_subnet" {
+  for_each = local.inbound_map
+
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value.cidr
+  availability_zone = each.value.az
+
+  tags = {
+    Name     = "cc-inbound-resolver-${each.value.az}"
+    Resolver = "inbound"
+  }
+}
+
 resource "aws_subnet" "cc_poise_outbound_endpoint_subnet" {
   for_each = local.poise_map
 
@@ -54,24 +67,28 @@ resource "aws_subnet" "cc_ncsc_outbound_endpoint_subnet" {
   }
 }
 
-resource "aws_subnet" "cc_inbound_endpoint_subnet" {
-  for_each = local.inbound_map
 
-  vpc_id            = var.vpc_id
-  cidr_block        = each.value.cidr
-  availability_zone = each.value.az
+##############################
+# Private NAT Gateway
+##############################
+
+
+resource "aws_nat_gateway" "private_nat_gw" {
+  for_each          = aws_subnet.cc_ncsc_outbound_endpoint_subnet
+  connectivity_type = "private"
+  subnet_id         = each.value.id
 
   tags = {
-    Name     = "cc-inbound-resolver-${each.value.az}"
-    Resolver = "inbound"
+    Name = "private-natgw-${each.key}"
   }
 }
+
 
 ##############################
 # Route Tables
 ##############################
 
-resource "aws_route_table" "cc_outbound_endpoints_rt" {
+resource "aws_route_table" "cc_poise_outbound_endpoint_rt" {
   vpc_id = var.vpc_id
 
   route {
@@ -80,9 +97,25 @@ resource "aws_route_table" "cc_outbound_endpoints_rt" {
   }
 
   tags = {
-    Name = "cc-outbound-resolver-endpoints-rt"
+    Name = "cc-poise-outbound-resolver-endpoints-rt"
   }
 }
+
+resource "aws_route_table" "cc_ncsc_outbound_endpoint_rt" {
+  for_each = aws_subnet.cc_ncsc_outbound_endpoint_subnet
+
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_nat_gw[each.key].id
+  }
+
+  tags = {
+    Name = "cc-ncsc-outbound-resolver-endpoints-rt-${each.key}"
+  }
+}
+
 
 resource "aws_route_table" "cc_inbound_resolver_rt" {
   vpc_id = var.vpc_id
@@ -105,15 +138,16 @@ resource "aws_route_table_association" "cc_poise_outbound_endpoint_assoc" {
   for_each = aws_subnet.cc_poise_outbound_endpoint_subnet
 
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.cc_outbound_endpoints_rt.id
+  route_table_id = aws_route_table.cc_poise_outbound_endpoint_rt.id
 }
 
 resource "aws_route_table_association" "cc_ncsc_outbound_endpoint_assoc" {
   for_each = aws_subnet.cc_ncsc_outbound_endpoint_subnet
 
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.cc_outbound_endpoints_rt.id
+  route_table_id = aws_route_table.cc_ncsc_outbound_endpoint_rt[each.key].id
 }
+
 
 resource "aws_route_table_association" "cc_inbound_resolver_assoc" {
   for_each = aws_subnet.cc_inbound_endpoint_subnet
