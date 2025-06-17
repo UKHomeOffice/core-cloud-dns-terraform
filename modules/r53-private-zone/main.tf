8839dbd3002f8b095c76fb9e05c60623ff8cda63
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2025-06-17 14:51:39 +0100

    dynamic vpc block

--- modules/r53-private-zone/main.tf
+++ modules/r53-private-zone/main.tf
@@ -2,18 +2,13 @@ resource "aws_route53_zone" "private_zone" {
   name    = "internal.${var.domain_name}"
   comment = "Private hosted zone"
 
-  vpc {
-    vpc_id     = var.vpc_id
-    vpc_region = "eu-west-2"
+  dynamic "vpc" {
+    for_each = var.vpc_ids
+    content {
+      vpc_id     = vpc.value
+      vpc_region = "eu-west-2"
+    }
   }
-
-  # dynamic "vpc" {
-  #   for_each = var.vpc_ids
-  #   content {
-  #     vpc_id     = vpc.value
-  #     vpc_region = "eu-west-2"
-  #   }
-  # }
 
   # dynamic "vpc" {
   #   for_each = try(tolist(lookup(each.value, "vpc", [])), [lookup(each.value, "vpc", {})])
