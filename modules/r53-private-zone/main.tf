9969bf2d4efa19ca25a95ec3bdfcd7c7643aa00f
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2025-06-17 15:23:11 +0100

    toset vpc_ids

--- modules/r53-private-zone/main.tf
+++ modules/r53-private-zone/main.tf
@@ -3,7 +3,7 @@ resource "aws_route53_zone" "private_zone" {
   comment = "Private hosted zone"
 
   dynamic "vpc" {
-    for_each = var.vpc_ids
+    for_each = toset(var.vpc_ids)
     content {
       vpc_id     = vpc.value
       vpc_region = "eu-west-2"
