bf487b904fdf50687527f61766a5dda224bbb28a
Juha Remes <juha.remes@digital.homeoffice.gov.uk>
2026-02-25 11:56:56 +0000

    Introduce KMS Key policy

--- modules/r53-public-zone/main.tf
+++ modules/r53-public-zone/main.tf
@@ -9,10 +9,40 @@ resource "aws_route53_zone" "workload_zone" {
   )
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "dnssec_key" {
   count               = var.enable_dnssec ? 1 : 0
   description         = "KMS key for DNSSEC signing for ${var.domain_name}"
   enable_key_rotation = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "Enable IAM User Permissions"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action    = "kms:*"
+        Resource  = "*"
+      },
+      {
+        Sid      = "Allow Route53 DNSSEC"
+        Effect   = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_route53_key_signing_key" "ksk" {
